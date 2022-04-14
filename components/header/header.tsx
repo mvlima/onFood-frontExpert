6e87d698696707efd1fd80c18a0d31a257fac018
mvlima <vieira.mvlima@gmail.com>
2022-04-14 17:35:02 -0300

    feat: added navbar to header

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,7 +1,24 @@
+import Link from "next/link";
+import Styles from "./header.module.css";
+
 export function Header() {
   return (
     <header>
-      <h1>Header</h1>
+      <nav className={Styles.nav}>
+        <div className={Styles.brand}>[Logo]</div>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/favoritos">
+              <a>Favoritos</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
