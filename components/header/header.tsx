42492099f36a20022883f24bb3479f2ee06b0d96
mvlima <vieira.mvlima@gmail.com>
2022-04-18 12:11:44 -0300

    feat: added logo

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import Styles from "./header.module.css";
 
@@ -5,7 +6,19 @@ export function Header() {
   return (
     <header>
       <nav className={Styles.nav}>
-        <div className={Styles.brand}>[Logo]</div>
+        <div className={Styles.brand}>
+          <Link href="/">
+            <a>
+              <Image
+                src="/images/logo.png"
+                alt="OnFood"
+                height={36}
+                width={45}
+              />
+              <span>OnFood</span>
+            </a>
+          </Link>
+        </div>
         <ul>
           <li>
             <Link href="/">
