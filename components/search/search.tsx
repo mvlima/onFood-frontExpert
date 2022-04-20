639072ef238826f75e8526a6134da1d16456af16
mvlima <vieira.mvlima@gmail.com>
2022-04-19 23:12:06 -0300

    feat: added search input

--- components/search/search.tsx
+++ components/search/search.tsx
@@ -1,7 +1,41 @@
+import { useState } from "react";
+import { Combobox } from "@headlessui/react";
+import Styles from "./search.module.css";
+
+const cities = [
+  "São Paulo, SP",
+  "Rio de Janeiro, RJ",
+  "Florianópolis, SC",
+  "Porto Alegre, RS",
+  "Belo Horizonte, MG",
+];
+
 export function Search() {
+  const [selectedCity, setSelectedCity] = useState(cities[0]);
+  const [query, setQuery] = useState("");
+
+  const filteredCities =
+    query === ""
+      ? cities
+      : cities.filter((city) => {
+          return city.toLowerCase().includes(query.toLowerCase());
+        });
+
   return (
-    <div className="bg-gray-200 rounded-xl">
-      <p>Selecione uma cidade para iniciar</p>
-    </div>
+    <Combobox
+      value={selectedCity}
+      onChange={setSelectedCity}
+      as="div"
+      className={Styles.search}
+    >
+      <Combobox.Input onChange={(event) => setQuery(event.target.value)} />
+      <Combobox.Options>
+        {filteredCities.map((city) => (
+          <Combobox.Option key={city} value={city}>
+            {city}
+          </Combobox.Option>
+        ))}
+      </Combobox.Options>
+    </Combobox>
   );
 }
